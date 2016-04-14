9b7d5d015b36d049a5e50b33c8b0e03764fdfe41
Roktaal <pdarko78@gmail.com>
2016-04-15 01:22:38 +0200

    1.0.0
    First version

---
+++ control.lua
@@ -0,0 +1,72 @@
+require("defines")
+
+local buttonName = nil
+local buttons = {
+	["upgrade-planner-config-button"] = true,
+	["blueprintTools"] = true,
+	["toggleTrainInfo"] = true,
+	["research_Q"] = true,
+}
+
+local flows = {
+	["fatControllerButtons"] = "toggleTrainInfo",
+}
+
+local technologies = {
+	["upgrade-planner-config-button"] = "automated-construction",
+	["blueprintTools"] = "automated-construction",
+	["toggleTrainInfo"] = "rail-signals",
+}
+
+function doButtons(player_index, research_name)
+	local player = game.players[player_index]
+	local modlist = player.gui.top.children_names
+	
+	for i = 1, #modlist do
+		if flows[modlist[i]] then
+			buttonName = flows[modlist[i]]
+		else
+			buttonName = modlist[i]
+		end
+		--game.player.print(buttonName)
+
+		if buttons[modlist[i]] or flows[modlist[i]] then
+			player.gui.top[modlist[i]].destroy()
+		end
+		if not player.gui.top[buttonName] then --and (player.force.technologies[technologies[buttonName]].researched or technologies[buttonName] == research_name) then
+			if technologies[buttonName] then
+				if player.force.technologies[technologies[buttonName]].researched or technologies[buttonName] == research_name then
+					player.gui.top.add({
+						type = "button",
+						name = buttonName,
+						style = "mod-iconizer_" .. buttonName
+					})
+				end
+			else
+				player.gui.top.add({
+					type = "button",
+					name = buttonName,
+					style = "mod-iconizer_" .. buttonName
+				})
+			end
+		end
+	end
+end
+
+script.on_event(defines.events.on_gui_click, function(event) 
+	local element = event.element
+	local player = game.get_player(event.player_index)
+	if buttons[element.name] then
+		player.gui.top[element.name].caption = ""
+	end
+end)
+
+script.on_event(defines.events.on_research_finished, function(event)
+	for player_index, player in pairs(game.players) do
+		doButtons(player_index, event.research.name)
+	end
+end)
+
+script.on_event(defines.events.on_player_created, function(event) doButtons(event.player_index, nil) end)
+script.on_configuration_changed(function(data) for i = 1, #game.players do doButtons(i, nil) end end)
+script.on_init(function() for i=1, #game.players do doButtons(i, nil) end end)
---
+++ prototypes/style.lua
@@ -0,0 +1,234 @@
+data:extend(
+	{
+	  {
+		type = "font",
+		name = "mod-iconizer_font",
+		from = "default",
+		size = 10
+	  }
+	}
+)
+
+data.raw["gui-style"].default["mod-iconizer_upgrade-planner-config-button"] =
+{
+    type = "button_style",
+    parent = "button_style",
+    width = 36,
+    height = 36,
+    top_padding = 6,
+    right_padding = 0,
+    bottom_padding = 0,
+    left_padding = 0,
+    font = "mod-iconizer_font",
+    default_graphical_set =
+    {
+        type = "monolith",
+        monolith_image =
+        {
+            filename = "__mod-iconizer__/graphics/mod-iconizer_upgrade-planner.png",
+            priority = "extra-high-no-scale",
+            width = 36,
+            height = 36,
+			x = 0,
+			y = 0,
+        }
+    },
+    hovered_graphical_set =
+    {
+        type = "monolith",
+        monolith_image =
+        {
+            filename = "__mod-iconizer__/graphics/mod-iconizer_upgrade-planner.png",
+            priority = "extra-high-no-scale",
+            width = 36,
+            height = 36,
+			x = 36,
+			y = 0,
+        }
+    },
+    clicked_graphical_set =
+    {
+        type = "monolith",
+        monolith_image =
+        {
+            filename = "__mod-iconizer__/graphics/mod-iconizer_upgrade-planner.png",
+            width = 36,
+            height = 36,
+			x = 36,
+			y = 0,
+        }
+    },
+	left_click_sound =
+	{
+		filename = "__core__/sound/gui-click.ogg",
+		volume = 1
+	}
+}
+
+data.raw["gui-style"].default["mod-iconizer_blueprintTools"] =
+{
+    type = "button_style",
+    parent = "button_style",
+    width = 36,
+    height = 36,
+    top_padding = 6,
+    right_padding = 0,
+    bottom_padding = 0,
+    left_padding = 0,
+    font = "mod-iconizer_font",
+    default_graphical_set =
+    {
+        type = "monolith",
+        monolith_image =
+        {
+            filename = "__mod-iconizer__/graphics/mod-iconizer_blueprintTools.png",
+            priority = "extra-high-no-scale",
+            width = 36,
+            height = 36,
+			x = 0,
+			y = 0,
+        }
+    },
+    hovered_graphical_set =
+    {
+        type = "monolith",
+        monolith_image =
+        {
+            filename = "__mod-iconizer__/graphics/mod-iconizer_blueprintTools.png",
+            priority = "extra-high-no-scale",
+            width = 36,
+            height = 36,
+			x = 36,
+			y = 0,
+        }
+    },
+    clicked_graphical_set =
+    {
+        type = "monolith",
+        monolith_image =
+        {
+            filename = "__mod-iconizer__/graphics/mod-iconizer_blueprintTools.png",
+            width = 36,
+            height = 36,
+			x = 36,
+			y = 0,
+        }
+    },
+	left_click_sound =
+	{
+		filename = "__core__/sound/gui-click.ogg",
+		volume = 1
+	}
+}
+
+data.raw["gui-style"].default["mod-iconizer_toggleTrainInfo"] =
+{
+    type = "button_style",
+    parent = "button_style",
+    width = 36,
+    height = 36,
+    top_padding = 6,
+    right_padding = 0,
+    bottom_padding = 0,
+    left_padding = 0,
+    font = "mod-iconizer_font",
+    default_graphical_set =
+    {
+        type = "monolith",
+        monolith_image =
+        {
+            filename = "__mod-iconizer__/graphics/mod-iconizer_toggleTrainInfo.png",
+            priority = "extra-high-no-scale",
+            width = 36,
+            height = 36,
+			x = 0,
+			y = 0,
+        }
+    },
+    hovered_graphical_set =
+    {
+        type = "monolith",
+        monolith_image =
+        {
+            filename = "__mod-iconizer__/graphics/mod-iconizer_toggleTrainInfo.png",
+            priority = "extra-high-no-scale",
+            width = 36,
+            height = 36,
+			x = 36,
+			y = 0,
+        }
+    },
+    clicked_graphical_set =
+    {
+        type = "monolith",
+        monolith_image =
+        {
+            filename = "__mod-iconizer__/graphics/mod-iconizer_toggleTrainInfo.png",
+            width = 36,
+            height = 36,
+			x = 36,
+			y = 0,
+        }
+    },
+	left_click_sound =
+	{
+		filename = "__core__/sound/gui-click.ogg",
+		volume = 1
+	}
+}
+
+data.raw["gui-style"].default["mod-iconizer_research_Q"] =
+{
+    type = "button_style",
+    parent = "button_style",
+    width = 36,
+    height = 36,
+    top_padding = 6,
+    right_padding = 0,
+    bottom_padding = 0,
+    left_padding = 0,
+    font = "mod-iconizer_font",
+    default_graphical_set =
+    {
+        type = "monolith",
+        monolith_image =
+        {
+            filename = "__mod-iconizer__/graphics/mod-iconizer_research_Q.png",
+            priority = "extra-high-no-scale",
+            width = 36,
+            height = 36,
+			x = 0,
+			y = 0,
+        }
+    },
+    hovered_graphical_set =
+    {
+        type = "monolith",
+        monolith_image =
+        {
+            filename = "__mod-iconizer__/graphics/mod-iconizer_research_Q.png",
+            priority = "extra-high-no-scale",
+            width = 36,
+            height = 36,
+			x = 36,
+			y = 0,
+        }
+    },
+    clicked_graphical_set =
+    {
+        type = "monolith",
+        monolith_image =
+        {
+            filename = "__mod-iconizer__/graphics/mod-iconizer_research_Q.png",
+            width = 36,
+            height = 36,
+			x = 36,
+			y = 0,
+        }
+    },
+	left_click_sound =
+	{
+		filename = "__core__/sound/gui-click.ogg",
+		volume = 1
+	}
+}
