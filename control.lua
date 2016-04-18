92672bbb718df67e01f540ee7932d1a5d900db26
Roktaal <pdarko78@gmail.com>
2016-04-19 01:50:01 +0200

    1.1.0
    - Fixed button changing in case of nested buttons
    - Added support for Orbital Ion Cannon Mod

--- control.lua
+++ control.lua
@@ -1,53 +1,51 @@
 require("defines")
 
-local buttonName = nil
 local buttons = {
-	["upgrade-planner-config-button"] = true,
-	["blueprintTools"] = true,
-	["toggleTrainInfo"] = true,
-	["research_Q"] = true,
+	["upgrade-planner-config-button"] = {type = "button", name = "upgrade-planner-config-button", technology = "automated-construction"},
+	["blueprintTools"] = {type = "button", name = "blueprintTools", technology = "automated-construction"},
+	["fatControllerButtons"] = {type = "flow", name = "toggleTrainInfo", technology = "rail-signals"},
+	["research_Q"] = {type = "button", name = "research_Q"},
+	["ion-cannon-button"] = {type = "button", name = "ion-cannon-button", technology = "rocket-silo"}
 }
 
-local flows = {
-	["fatControllerButtons"] = "toggleTrainInfo",
-}
-
-local technologies = {
-	["upgrade-planner-config-button"] = "automated-construction",
-	["blueprintTools"] = "automated-construction",
-	["toggleTrainInfo"] = "rail-signals",
-}
+function message(mes)
+	for i, player in pairs(game.players) do
+		player.print(mes)
+	end
+end
 
 function doButtons(player_index, research_name)
-	local player = game.players[player_index]
+	local player = game.get_player(player_index)
 	local modlist = player.gui.top.children_names
+	local element
+	local buttonName
 	
 	for i = 1, #modlist do
-		if flows[modlist[i]] then
-			buttonName = flows[modlist[i]]
-		else
-			buttonName = modlist[i]
-		end
-		--game.player.print(buttonName)
+		element = player.gui.top[modlist[i]]
 
-		if buttons[modlist[i]] or flows[modlist[i]] then
-			player.gui.top[modlist[i]].destroy()
-		end
-		if not player.gui.top[buttonName] then --and (player.force.technologies[technologies[buttonName]].researched or technologies[buttonName] == research_name) then
-			if technologies[buttonName] then
-				if player.force.technologies[technologies[buttonName]].researched or technologies[buttonName] == research_name then
-					player.gui.top.add({
-						type = "button",
-						name = buttonName,
-						style = "mod-iconizer_" .. buttonName
-					})
+		if buttons[element.name] then
+			if buttons[element.name].type ~= "flow" then
+				if buttons[element.name].technology then
+					if (research_name ~= nil and research_name == buttons[element.name].technology) or player.force.technologies[buttons[element.name].technology].researched then
+						player.gui.top[element.name].caption = ""
+						player.gui.top[element.name].style = "mod-iconizer_" .. element.name
+					end
+				else
+					player.gui.top[element.name].caption = ""
+					player.gui.top[element.name].style = "mod-iconizer_" .. element.name
 				end
 			else
-				player.gui.top.add({
-					type = "button",
-					name = buttonName,
-					style = "mod-iconizer_" .. buttonName
-				})
+				buttonName = buttons[element.name].name
+				
+				if buttons[element.name].technology then
+					if (research_name ~= nil and research_name == buttons[element.name].technology) or player.force.technologies[buttons[element.name].technology].researched then
+						player.gui.top[element.name][buttonName].caption = ""
+						player.gui.top[element.name][buttonName].style = "mod-iconizer_" .. buttonName
+					end
+				else
+					player.gui.top[element.name][buttonName].caption = ""
+					player.gui.top[element.name][buttonName].style = "mod-iconizer_" .. buttonName
+				end
 			end
 		end
 	end
@@ -56,8 +54,13 @@ end
 script.on_event(defines.events.on_gui_click, function(event) 
 	local element = event.element
 	local player = game.get_player(event.player_index)
+	
 	if buttons[element.name] then
 		player.gui.top[element.name].caption = ""
+		player.gui.top[element.name].style = "mod-iconizer_" .. element.name
+	elseif buttons[element.parent.name] and buttons[element.parent.name].name == element.name then
+		player.gui.top[element.parent.name][element.name].caption = ""
+		player.gui.top[element.parent.name][element.name].style = "mod-iconizer_" .. element.name
 	end
 end)
 
@@ -69,4 +72,4 @@ end)
 
 script.on_event(defines.events.on_player_created, function(event) doButtons(event.player_index, nil) end)
 script.on_configuration_changed(function(data) for i = 1, #game.players do doButtons(i, nil) end end)
-script.on_init(function() for i=1, #game.players do doButtons(i, nil) end end)
+script.on_init(function() for i = 1, #game.players do doButtons(i, nil) end end)
